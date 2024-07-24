44f68af457e09d142ba1e42e15df363e6ab8e177
Pranith Gunda <pranith0527@yahoo.com>
2024-07-24 15:47:12 -0400

    Defined handlebar 'blogfeedback' to display feedback added to blog, added JS functionality to save feedback given to blog

--- public/js/feedback.js
+++ public/js/feedback.js
@@ -2,9 +2,31 @@ const feedbackBtnEl = document.getElementById('feedbackBtn');
 const feedbackEl = document.getElementById('feedback')
 
 // event listener to listen for user submitted feedback
-feedbackBtnEl.addEventListener('click',(event)=>{
-    // disable feedback text area
-    feedbackEl.disabled = true;
-    feedbackBtnEl.classList.add('hidden');
-    // Call api to add feedback to post
+feedbackBtnEl.addEventListener('click', async (event) => {
+    const targetEl = event.target;
+    const blogId = targetEl.getAttribute('data-blogid');
+    if(blogId && feedbackEl.value){
+        const originLocation = window.origin;
+        await fetch(`${originLocation}/api/blog/feedback`,{
+            method:'POST',
+            body:JSON.stringify({
+                comment:feedbackEl.value,
+                blog_id:blogId
+            }),
+            headers:{
+                'Content-Type':'application/json',
+            },
+        })
+        .then((response)=>{
+            if(response.ok){
+                document.location.replace(`${originLocation}/blog/feedback/${blogId}`)
+            }
+        })
+        .catch((err)=>{
+            console.error(err);
+        })
+    }
+    else{
+        window.alert('Please enter comment before you submit');
+    }
 })
